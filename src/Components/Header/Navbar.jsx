928b0ac9d6e0cacf9ddf1a2935a8f7b0b4ac7c48
Spencer D <spencer.dedrick@gmail.com>
2022-02-05 22:23:30 -0600

    Repository initiated. Start of project.

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
-const NavWrapper = styled.nav``;
+const NavWrapper = styled.nav`
+  width: 100vw;
+  background-color: #232323;
+  height: 65px;
+`;
 
 const Navbar = () => {
-  return (
-    <NavWrapper>
-      <h1>hello</h1>
-    </NavWrapper>
-  );
+  return <NavWrapper></NavWrapper>;
 };
 
 export default Navbar;
